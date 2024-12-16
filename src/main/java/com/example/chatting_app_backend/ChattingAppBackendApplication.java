4b3ee07236d9d3daf059fba20c9bdcdadc1c83d5
SahilDhanwani <sahildhanwani291203@gmail.com>
2024-12-17 00:30:22 +0530

    Minor change

--- src/main/java/com/example/chatting_app_backend/ChattingAppBackendApplication.java
+++ src/main/java/com/example/chatting_app_backend/ChattingAppBackendApplication.java
@@ -1,13 +1,12 @@
-package com.SahilD.ChattingApplication;
+package com.example.chatting_app_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChattingApplication {
-	
-	public static void main(String[] args) {
-		SpringApplication.run(ChattingApplication.class, args);
-	}
+public class ChattingAppBackendApplication {
 
+	public static void main(String[] args) {
+		SpringApplication.run(ChattingAppBackendApplication.class, args);
+	}
 }
--- src/test/java/com/example/chatting_app_backend/ChattingAppBackendApplicationTests.java
+++ src/test/java/com/example/chatting_app_backend/ChattingAppBackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.SahilD.ChattingApplication;
+package com.example.chatting_app_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ChattingApplicationTests {
+class ChattingAppBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
