34e3e4ba1623ab7a583d49e8f82a51eea3dc9d89
SahilDhanwani <sahildhanwani291203@gmail.com>
2024-12-28 23:00:40 +0530

    Refactor WebSocketConfig to use MessageBrokerRegistry for message broker configuration

--- src/main/java/com/example/chatting_app_backend/configurations/WebSocketConfig.java
+++ src/main/java/com/example/chatting_app_backend/configurations/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.example.chatting_app_backend.configurations;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -18,9 +19,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
 
     @Override
-    public void configureMessageBroker(@SuppressWarnings("null") org.springframework.messaging.simp.config.MessageBrokerRegistry registry) {
+    public void configureMessageBroker(@SuppressWarnings("null") MessageBrokerRegistry registry) {
         // Message broker configuration
         registry.enableSimpleBroker("/topic"); // Set up /topic for broadcasting messages
         registry.setApplicationDestinationPrefixes("/app"); // Prefix for application messages
     }
 }
+
