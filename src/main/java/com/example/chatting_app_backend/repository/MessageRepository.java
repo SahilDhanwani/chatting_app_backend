dc1ec2fd7034c58cbba9cc2aa0b9e6d647611334
SahilDhanwani <sahildhanwani291203@gmail.com>
2025-01-25 13:26:01 +0530

    Fix message retrieval query to correctly match sender and receiver IDs

--- src/main/java/com/example/chatting_app_backend/repository/MessageRepository.java
+++ src/main/java/com/example/chatting_app_backend/repository/MessageRepository.java
@@ -13,6 +13,6 @@ import com.example.chatting_app_backend.model.messages;
 public interface MessageRepository extends JpaRepository<messages, Integer> {
 
     @Query("SELECT new com.example.chatting_app_backend.data_packets.Responses.GetMessagesResponse(m.message, m.sender_id) " +
-    "FROM messages m WHERE (m.sender_id = ?1 OR m.sender_id = ?2) AND (m.receiver_id = ?2 OR m.receiver_id = ?1)")
+    "FROM messages m WHERE (m.sender_id = ?1 AND m.receiver_id = ?2) OR (m.sender_id = ?2 AND m.receiver_id = ?1)")
     public List<GetMessagesResponse> findMessages(int user1, int user2);
 }
